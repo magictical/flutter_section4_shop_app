a6957a9765f18853f557b8b0d354a6d39b54abc5
IL KWON KIM <magictical@gmail.com>
2020-01-04 22:59:57 +0900

    display productoverview screen and each item in the list

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Product {
   final String id;
   final String title;
-  final String decsription;
+  final String description;
   final double price;
   final String imageUrl;
   bool isFavorite;
@@ -11,7 +11,7 @@ class Product {
   Product({
     @required this.id,
     @required this.title,
-    @required this.decsription,
+    @required this.description,
     @required this.price,
     @required this.imageUrl,
     this.isFavorite,
