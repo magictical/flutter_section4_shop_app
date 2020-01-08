ba6c2c8064761dc74254fa4d8fe964df40717a7d
IL KWON KIM <magictical@gmail.com>
2020-01-08 23:29:42 +0900

    use Consumer For the performance which rebuild its child only

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,38 +12,47 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return GridTile(
-      child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
-                arguments: product.id);
-          },
-          child: Image.network(product.imageUrl)),
-      footer: ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTileBar(
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
-            ),
-            onPressed: () {
-              product.toggleFavoriteStatus();
+    // Provider will detect the change and call build method
+    // this my inefficient, Use Consumer which only rebuild its child if there is change
+    final product = Provider.of<Product>(context, listen: false);
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: product.id,
+              );
             },
-            color: Theme.of(context).accentColor,
-          ),
-          title: Text(
-            product.title,
-            textAlign: TextAlign.center,
-          ),
-          trailing: IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
+            child: Image.network(product.imageUrl)),
+        footer: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: GridTileBar(
+            leading: Consumer<Product>(
+              builder: (ctn, product, child) => IconButton(
+                icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                ),
+                onPressed: () {
+                  product.toggleFavoriteStatus();
+                },
+                color: Theme.of(context).accentColor,
+              ),
             ),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
+            backgroundColor: Colors.black87,
           ),
-          backgroundColor: Colors.black87,
         ),
       ),
     );
