176cf91c7d12aedd5157296725a72152a62f018b
IL KWON KIM <magictical@gmail.com>
2020-01-24 21:27:28 +0900

    use http and post products data to Fireabse DB

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import './product.dart';
 
@@ -67,6 +69,20 @@ class Products with ChangeNotifier {
   // }
 
   void addProduct(Product product) {
+    // url for 'Products' in firebase
+    const url = 'https://flutter-update-97117.firebaseio.com/products.json';
+    // create json
+    http.post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'imageUrl': product.imageUrl,
+        'price': product.price,
+        'isFavorite': product.isFavorite,
+      }),
+    );
+
     final newProduct = Product(
       title: product.title,
       description: product.description,
