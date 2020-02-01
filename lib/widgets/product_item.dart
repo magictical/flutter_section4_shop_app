5b34cc33e44dbcd9661dcb5b5181d86be077cb07
IL KWON KIM <magictical@gmail.com>
2020-02-02 02:27:49 +0900

    fix bugs in image display on the overview screen

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -22,57 +22,58 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(
-                ProductDetailScreen.routeName,
-                arguments: product.id,
-              );
-            },
-            child: Image.network(product.imageUrl)),
-        footer: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: GridTileBar(
-            leading: Consumer<Product>(
-              builder: (ctn, product, child) => IconButton(
-                icon: Icon(
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border,
-                ),
-                onPressed: () {
-                  product.toggleFavoriteStatus();
-                },
-                color: Theme.of(context).accentColor,
-              ),
-            ),
-            title: Text(
-              product.title,
-              textAlign: TextAlign.center,
-            ),
-            trailing: IconButton(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (ctn, product, child) => IconButton(
               icon: Icon(
-                Icons.shopping_cart,
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
               ),
               onPressed: () {
-                cart.addItem(product.id, product.price, product.title);
-                // hide previous one when display the new one
-                Scaffold.of(context).hideCurrentSnackBar();
-                // 위젯트리에서 가장 가까운 Scaffold의 context를 사용 여기서는 product overview임
-                Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text(
-                    'Added Item To Cart!',
-                    textAlign: TextAlign.center,
-                  ),
-                  duration: Duration(seconds: 2),
-                  action: SnackBarAction(
-                    label: 'UNDO',
-                    onPressed: () {
-                      cart.removeSingleItem(product.id);
-                    },
-                  ),
-                ));
+                product.toggleFavoriteStatus();
               },
               color: Theme.of(context).accentColor,
             ),
-            backgroundColor: Colors.black87,
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              // hide previous one when display the new one
+              Scaffold.of(context).hideCurrentSnackBar();
+              // 위젯트리에서 가장 가까운 Scaffold의 context를 사용 여기서는 product overview임
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Added Item To Cart!',
+                  textAlign: TextAlign.center,
+                ),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'UNDO',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
+            },
+            color: Theme.of(context).accentColor,
           ),
         ),
       ),
