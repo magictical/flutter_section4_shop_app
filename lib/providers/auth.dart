d3ac6a8f35b8181a0275e410e3e818512d3c5032
IL KWON KIM <magictical@gmail.com>
2020-02-07 23:57:59 +0900

    add login function

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -9,10 +9,30 @@ class Auth with ChangeNotifier {
   String _userId;
 
   Future<void> signUp(String email, String password) async {
+    // get apikey from Firebase
     final apiKey = ApiKeyStore.fireBaseKey;
     print(apiKey);
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=$apiKey';
+
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
+
+  Future<void> login(String email, String password) async {
+    final apiKey = ApiKeyStore.fireBaseKey;
+    print(apiKey);
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$apiKey';
 
     final response = await http.post(
       url,
