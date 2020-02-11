f69e65eefafc3c8245c240051957f9af780fc2c9
IL KWON KIM <magictical@gmail.com>
2020-02-12 00:35:49 +0900

    error handling while login and signup

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/http_exception.dart';
 import '../config.dart';
 
 class Auth with ChangeNotifier {
@@ -15,24 +16,33 @@ class Auth with ChangeNotifier {
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:${urlSegment}key=${apiKey}';
 
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      print(responseData);
+
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signUp(String email, String password) async {
-    _authenticate(email, password, 'signUp?');
+    return _authenticate(email, password, 'signUp?');
   }
 
   Future<void> login(String email, String password) async {
-    _authenticate(email, password, 'signInWithPassword?');
+    return _authenticate(email, password, 'signInWithPassword?');
   }
 }
